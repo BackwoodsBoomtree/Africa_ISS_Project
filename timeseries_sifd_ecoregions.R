4cadf3e68ca64d16b3792daf4999e4d79f54466b
Doughty <russell.doughty@ou.edu>
2022-10-13 10:31:25 -0500

    Comments

--- timeseries_sifd_ecoregions.R
+++ timeseries_sifd_ecoregions.R
@@ -1,10 +1,10 @@
 
-csv_list <- list.files("G:/Africa/csv", pattern = "*.csv", full.names = TRUE)
+csv_list <- list.files("G:/Africa/csv/ecoregions/mask_Dans", pattern = "*.csv", full.names = TRUE)
 x_labs   <- format(seq(as.Date("2019/1/1"), as.Date("2021/12/1"), "month"), "%Y-%m")
 x        <- 1:36
 err_col  <- "#8ab8e6"
 
-cairo_pdf("G:/Africa/figs/timeseries_sifd_ecoregions.pdf", width = 20, height = 15)
+cairo_pdf("G:/Africa/figs/timeseries_sifd_ecoregions_2.5km_mask.pdf", width = 20, height = 15)
 
 par(mfrow = c(5, 3), oma=c(2.0,0.1,1.25,0.1))
 
@@ -19,10 +19,14 @@ for (i in 1:length(csv_list)) {
   
   plot(x, sifd, type = "l", main = tit, ylab = "SIFdaily", axes = FALSE, xlab = NA)
   
-  if(i != 8) {
-    polygon(c(x, rev(x)), c(sifd+sem, rev(sifd-sem)), col = err_col, border = NA)
-    lines(x, sifd)
-  }
+  # This code was specific to data using MCD12 90% filter where there were months with no data
+  # if(i != 8) {
+  #   polygon(c(x, rev(x)), c(sifd+sem, rev(sifd-sem)), col = err_col, border = NA)
+  #   lines(x, sifd)
+  # }
+  
+  polygon(c(x, rev(x)), c(sifd+sem, rev(sifd-sem)), col = err_col, border = NA)
+  lines(x, sifd)
 
   axis(1, labels = x_labs, at =  c(1:36), tck = 0.03, mgp=c(3, 0.2, 0), las = 2)
   axis(2, labels = TRUE, tck = 0.03, mgp=c(3, 0.2, 0), las = 2)
