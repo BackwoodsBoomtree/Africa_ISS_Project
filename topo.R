29e09347e2fcffe660e801faacd8933df5ab6981
Doughty <russell.doughty@ou.edu>
2023-07-07 21:57:41 -0500

    working

---
+++ topo.R
@@ -0,0 +1,26 @@
+library(terra)
+library(viridis)
+
+ecos         <- vect("G:/Africa/Tropical_Africa_Ecoregions/eco_regions_of_interest.shp")
+evi_max      <- rast("G:/MCD43A4/vis/africa/min_max_monthly/max_monthly_2019-2021_EVI.nc")
+lc_mask      <- vect("G:/Africa/Forest_Masks/dissolved/Africa_merged_2019_2.5km_Buffer.shp")
+topo_files   <- list.files("G:/Topography/africa/median", pattern = "*.tif", full.names = TRUE)
+
+for (i in 1:length(topo_files)) {
+  tile <- rast(topo_files[i])
+  
+  if (i == 1) {
+    topo <- tile
+  } else {
+    topo <- mosaic(topo, tile, fun = "max")
+  }
+}
+
+topo <- mask(topo, evi_max)
+topo <- mask(topo, ecos)
+topo <- mask(topo, lc_mask)
+
+# topo[topo < 500] <- 500
+topo[topo > 1000] <- 1000
+
+plot(topo, col = rev(viridis(10)))
